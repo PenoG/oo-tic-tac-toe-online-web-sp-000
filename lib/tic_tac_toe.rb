4335517c6f862d4f78789f26526c322793747994
Mason Phillips <mjphillips2000@gmail.com>
2020-10-31 07:55:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,11 +44,11 @@ class TicTacToe
       true
     end
   end
-def valid_move?(board, index)
-  if index.between?(0,8) 
-    if !position_taken?(board, index)
-      true
+  def valid_move?(board, index)
+    if index.between?(0,8) 
+     if !position_taken?(board, index)
+        true
+      end
     end
   end
-end
 end
